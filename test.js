b1e701a589278a625177d6d224b5da03be401bed
eco9999 <emrecanocak@windowslive.com>
2019-12-18 03:20:11 +0300

    deleteDataBase() func test case imp.

--- test.js
+++ test.js
@@ -6,10 +6,15 @@ let input = {"event":"test","no":13};
 
 // main.writeToDataBase(name, dir, input, ()=>{});
 
-main.deleteFromDataBase(name, dir, 'name', 'emre', (err) => {
-	main.getFromDataBase(name, dir, 0, 0, (err, arr, len) => {
-		console.log(err);
-		console.log(arr);
-		console.log(len);
-	});
-})
+// main.deleteFromDataBase(name, dir, 'name', 'emre', (err) => {
+// 	main.getFromDataBase(name, dir, 0, 0, (err, arr, len) => {
+// 		console.log(err);
+// 		console.log(arr);
+// 		console.log(len);
+// 	});
+// })
+
+
+main.deleteDataBase(name, dir, (err)=>{
+	console.log(err)
+});
