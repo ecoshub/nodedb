43a0a9ec6e2285cfd3cdfd602ed20c24843329e5
eco9999 <emrecanocak@windowslive.com>
2019-12-24 16:19:19 +0300

    standart test change

--- test.js
+++ test.js
@@ -2,45 +2,46 @@ const main = require('./main.js')
 
 let name = 'test.json';
 let dir = '';
-let input = {"event":"test","no":13};
+let input = {"event":"test","no":0};
 
-const arr = [
-  {
-    "dbName": "virtualmint:08:00:27:fc:3d:f2",
-    "userIdentity": "virtualmint:08:00:27:fc:3d:f2",
-    "mac": "08:00:27:fc:3d:f2",
-    "from": "MYPC",
-    "to": "virtualmint",
-    "content": "1",
-    "contentType": "text",
-    "createdAt": "Dec 22,2019 - 16:53:00",
-    "uuid": "539e2470-f3f1-4409-8ea9-8039531a9ee1"
-  },
-  {
-    "dbName": "virtualmint:08:00:27:fc:3d:f2",
-    "userIdentity": "virtualmint:08:00:27:fc:3d:f2",
-    "mac": "08:00:27:fc:3d:f2",
-    "from": "MYPC",
-    "to": "virtualmint",
-    "content": "1",
-    "contentType": "text",
-    "createdAt": "Dec 22,2019 - 16:53:00",
-    "uuid": "539e2470-f3f1-4409-8ea9-8039531a9ee1"
-  },
-  {
-    "dbName": "virtualmint:08:00:27:fc:3d:f2",
-    "userIdentity": "virtualmint:08:00:27:fc:3d:f2",
-    "mac": "08:00:27:fc:3d:f2",
-    "from": "MYPC",
-    "to": "virtualmint",
-    "content": "1",
-    "contentType": "text",
-    "createdAt": "Dec 22,2019 - 16:53:00",
-    "uuid": "539e2470-f3f1-4409-8ea9-8039531a9ee1"
-  }
-]
+// const arr = [
+//   {
+//     "dbName": "virtualmint:08:00:27:fc:3d:f2",
+//     "userIdentity": "virtualmint:08:00:27:fc:3d:f2",
+//     "mac": "08:00:27:fc:3d:f2",
+//     "from": "MYPC",
+//     "to": "virtualmint",
+//     "content": "1",
+//     "contentType": "text",
+//     "createdAt": "Dec 22,2019 - 16:53:00",
+//     "uuid": "539e2470-f3f1-4409-8ea9-8039531a9ee1"
+//   },
+//   {
+//     "dbName": "virtualmint:08:00:27:fc:3d:f2",
+//     "userIdentity": "virtualmint:08:00:27:fc:3d:f2",
+//     "mac": "08:00:27:fc:3d:f2",
+//     "from": "MYPC",
+//     "to": "virtualmint",
+//     "content": "1",
+//     "contentType": "text",
+//     "createdAt": "Dec 22,2019 - 16:53:00",
+//     "uuid": "539e2470-f3f1-4409-8ea9-8039531a9ee1"
+//   },
+//   {
+//     "dbName": "virtualmint:08:00:27:fc:3d:f2",
+//     "userIdentity": "virtualmint:08:00:27:fc:3d:f2",
+//     "mac": "08:00:27:fc:3d:f2",
+//     "from": "MYPC",
+//     "to": "virtualmint",
+//     "content": "1",
+//     "contentType": "text",
+//     "createdAt": "Dec 22,2019 - 16:53:00",
+//     "uuid": "539e2470-f3f1-4409-8ea9-8039531a9ee1"
+//   }
+// ]
 
-// main.writeToDataBase(name, dir, input, ()=>{});
+const arr = [{"event":"test","no":1},{"event":"test","no":2},{"event":"test","no":3},{"event":"test","no":4}];
+main.writeToDataBaseArray(name, dir, arr, ()=>{});
 
 // main.deleteFromDataBase(name, dir, 'name', 'emre', (err) => {
 // 	main.getFromDataBase(name, dir, 0, 0, (err, arr, len) => {
@@ -51,9 +52,13 @@ const arr = [
 // })
 
 // main.writeToDataBaseArray(name, dir, arr, ()=>{
- main.getFromDataBase(name, dir, 0, 20, (err, arr, len) => {
-     console.log(err);
-     console.log(arr);
-     console.log(len);
- });
+ // main.getFromDataBase(name, dir, 0, 20, (err, arr, len) => {
+ //     console.log(err);
+ //     console.log(arr);
+ //     console.log(len);
+ // });
 // });
+
+// const arr1 = [{"event":"test","no":2}]
+
+// console.log(arr1.conca)
