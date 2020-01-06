2cd2a876418d8660740aaa58318c41c066c41be3
eco9999 <emrecanocak@windowslive.com>
2020-01-06 09:48:03 +0300

    standart test change

--- test.js
+++ test.js
@@ -40,8 +40,8 @@ let input = {"event":"test","no":0};
 //   }
 // ]
 
-const arr = [{"event":"test","no":1},{"event":"test","no":2},{"event":"test","no":3},{"event":"test","no":4}];
-main.writeToDataBaseArray(name, dir, arr, ()=>{});
+// const arr = [{"event":"test","no":1},{"event":"test","no":2},{"event":"test","no":3},{"event":"test","no":4}];
+// main.writeToDataBaseArray(name, dir, arr, ()=>{});
 
 // main.deleteFromDataBase(name, dir, 'name', 'emre', (err) => {
 // 	main.getFromDataBase(name, dir, 0, 0, (err, arr, len) => {
@@ -61,4 +61,25 @@ main.writeToDataBaseArray(name, dir, arr, ()=>{});
 
 // const arr1 = [{"event":"test","no":2}]
 
-// console.log(arr1.conca)
+
+// const DatabaseMessageContextProvider = props => {
+//   const [messageFromDatabase, dispatch] = useReducer(databaseMessageReducer, [], () => {
+//     main.getFromDataBase(name, dir, 0, 20, (err, arr, len) => {
+// 	    if (len === -1) {
+// 	      return null;
+// 	    }
+// 	    return arr
+// 	  });
+
+//   });
+
+//   const [messageFromDatabase, dispatch] = useReducer(databaseMessageReducer, [], () => {
+//     let data = [];
+
+//     const localData = localStorage.getItem('messages');
+//     return localData ? JSON.parse(localData) : [];
+//   });
+
+
+var result = main.getFromDataBaseSync(name, dir, 0, 10);
+console.log(result)
